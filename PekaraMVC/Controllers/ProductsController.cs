b0888c618b92813a5686e2b9603d4d0bd524550a
42444 <nstankovic4444@gmail.com>
2022-11-25 15:09:30 +0100

    Domaci

---
+++ PekaraMVC/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using PekaraMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PekaraMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = Details();
+            return View(products);
+        }
+
+        public List<Product> Details()
+        {
+            string jsonFromFile= System.IO.File.ReadAllText("Products.json");
+            List<Product> productsFromFile = JsonConvert.DeserializeObject<List<Product>>(jsonFromFile);
+
+            return productsFromFile;
+
+        }
+    }
+}
